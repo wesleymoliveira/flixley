c2eedf2e622eb2e45d4b983a734848f497b696be
Wesley M. Oliveira <oliveirawesleyrj@gmail.com>
2022-08-09 11:06:56 -0300

    setup basic routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
 
-const App = () => {
+import { Actors, Movies, MovieInformation, NavBar, Profile } from '.';
+
+function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <CssBaseline />
+      <NavBar />
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/movies/:id" element={<MovieInformation />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+          <Route path="*" element={(<p>There&apos;s nothing here!</p>)} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
