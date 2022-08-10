d07805334439ef846231831f6c8ec4ce258f17f5
Wesley M. Oliveira <oliveirawesleyrj@gmail.com>
2022-08-10 09:49:36 -0300

    setup style mode

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
+import useStyles from './styles';
 
 import { Actors, Movies, MovieInformation, NavBar, Profile } from '.';
 
 function App() {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
       <NavBar />
-      <main>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Routes>
           <Route exact path="/" element={<Movies />} />
           <Route exact path="/movies/:id" element={<MovieInformation />} />
