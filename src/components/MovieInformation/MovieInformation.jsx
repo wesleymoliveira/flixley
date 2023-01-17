5900416b5dc43dc70c7d7dedc4ba3b1fda40d550
Wesley M. Oliveira <oliveirawesleyrj@gmail.com>
2023-01-16 21:16:04 -0300

    init MovieInformation component

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,8 +1,78 @@
 import React from 'react';
+import { Modal, Typography, Button, ButtonGroup, Grid, Box, CircularProgress, useMediaQuery, Rating, ListItemIcon } from '@mui/material';
+import { Movie as MovieIcon, Theaters, Language, PlusOne, Favorite, FavoriteBorderOutlined, Remove, ArrowBack } from '@mui/icons-material';
+import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+
+import { selectGenreIdOrCategoryName } from '../../features/currentGenreIdOrCategoryName';
+
+import useStyles from './styles';
+import genreIcons from '../../assets/genres';
+import { useGetMovieQuery } from '../../services/TMDB';
 
 function MovieInformation() {
+  const { id } = useParams();
+  const { data, isFetching, error } = useGetMovieQuery(id);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  if (isFetching) {
+    return (
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <CircularProgress size="8rem" />;
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Link to="/">Go back, something has gone wrong</Link>
+      </Box>
+    );
+  }
+
   return (
-    <div>MovieInformation</div>
+    <Grid container className={classes.containerSpaceRound}>
+      <Grid item sm={12} lg={4}>
+        <img className={classes.poster} src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`} alt={data?.title} />
+
+      </Grid>
+      <Grid item container direction="column" lg={7}>
+        <Typography variant="h3" align="center" gutterBottom>
+          {data?.title} ({data.release_date.split('-')[0]})
+        </Typography>
+        <Typography variant="h5" align="center" gutterBottom>
+          {data?.tagline}
+        </Typography>
+        <Grid item className={classes.containerSpaceRound}>
+          <Box display="flex" align="center">
+            <Rating readOnly value={data.vote_average / 2} />
+            <Typography style={{ marginLeft: '10px' }} variant="subtitle1" gutterBottom>
+
+              {data?.vote_average} / 10
+            </Typography>
+          </Box>
+          <Typography variant="h6" align="center" gutterBottom>
+
+            {data?.runtime} min / {data?.spoken_languages.lenght > 1 ? data?.spoken_languages.map((language) => language.name).join(', ') : data?.spoken_languages[0].name}
+          </Typography>
+        </Grid>
+        <Grid item className={classes.genresContainer}>
+          {data?.genres?.map((genre, i) => (
+            <Link onClick={() => dispatch(selectGenreIdOrCategoryName(genre.id))} className={classes.links} key={i} to="/">
+              <ListItemIcon>
+                <img src={`${genreIcons[genre.name.toLowerCase()]}`} className={classes.genreImage} height={30} />
+                <Typography color="textPrimary" variant="subtitle1">
+                  {genre?.name}
+                </Typography>
+              </ListItemIcon>
+            </Link>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
---
+++ src/components/MovieInformation/styles.js
@@ -0,0 +1,50 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles((theme) => ({
+  containerSpaceRound: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    margin: '10px 0 !important',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+      flexWrap: 'wrap',
+    },
+  },
+  poster: {
+    borderRadius: '20px',
+    boxShadow: '0.5em 1em 1em rgb(64, 64, 70)',
+    width: '80%',
+    [theme.breakpoints.down('md')]: {
+      margin: '0 auto',
+      width: '50%',
+      height: '350px',
+    },
+    [theme.breakpoints.down('sm')]: {
+      margin: '0 auto',
+      width: '100%',
+      height: '350px',
+      marginbottom: '30px',
+    },
+  },
+  links: {
+    color: theme.palette.text.primary,
+    textDecoration: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    [theme.breakpoints.down('sm')]: {
+      padding: '0.5rem 1rem',
+    },
+  },
+  genresContainer: {
+    margin: '10px 0 !important',
+    display: 'flex',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+  },
+  genreImage: {
+    filter: theme.palette.mode === 'dark' && 'invert(1)',
+    marginRight: '10px',
+    marginLeft: '10px',
+  },
+}));
